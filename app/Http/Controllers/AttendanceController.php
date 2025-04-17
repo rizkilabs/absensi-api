32dcd86c1e8b43716d85347da54d050c9523eac5
rizkilabs <rizkilabs.dev@gmail.com>
2025-04-18 06:53:18 +0700

    add location validation (gps radius)

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -5,8 +5,32 @@ namespace App\Http\Controllers;
 use App\Models\Attendance;
 use Illuminate\Http\Request;
 
+const OFFICE_LAT = -6.210656810621744;
+const OFFICE_LNG = 106.81294239422382;
+const MAX_DISTANCE = 100; // meters
+
 class AttendanceController extends Controller
 {
+
+
+    private function calculateDistance($lat1, $lng1, $lat2, $lng2)
+    {
+        $earthRadius = 6371000; // dalam meter
+
+        $dLat = deg2rad($lat2 - $lat1);
+        $dLng = deg2rad($lng2 - $lng1);
+
+        $a = sin($dLat / 2) * sin($dLat / 2) +
+            cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
+            sin($dLng / 2) * sin($dLng / 2);
+
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        $distance = $earthRadius * $c;
+
+        return $distance;
+    }
+
+
     public function checkIn(Request $request)
     {
         $request->validate([
@@ -23,6 +47,20 @@ class AttendanceController extends Controller
             return response()->json(['message' => 'Already checked in today'], 400);
         }
 
+
+        $distance = $this->calculateDistance(
+            $request->latitude,
+            $request->longitude,
+            OFFICE_LAT,
+            OFFICE_LNG
+        );
+
+
+        if ($distance > MAX_DISTANCE) {
+            return response()->json(['message' => 'You are too far from the office (' .  round($distance) .  ' meter',], 400);
+        }
+
+
         $attendance = Attendance::create([
             'user_id' => $request->user()->id,
             'check_in' => now(),
@@ -30,7 +68,7 @@ class AttendanceController extends Controller
             'longitude' => $request->longitude,
         ]);
 
-        return response()->json(['message' => 'Checked in successfully', 'date' => $attendance], 200);
+        return response()->json(['message' => 'Checked in successfully (' . round($distance) . ' meter)', 'date' => $attendance], 200);
     }
 
     public function checkOut(Request $request)
@@ -58,7 +96,7 @@ class AttendanceController extends Controller
 
     public function history(Request $request)
     {
-        $history = Attendance::where('user_id', auth()->id())
+        $history = Attendance::where('user_id', auth()->id)
             ->orderBy('check_in', 'desc')
             ->get();
         return response()->json($history, 200);
