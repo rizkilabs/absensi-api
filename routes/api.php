1402efcab0e73ff37c9f6b324bc44b22b363a101
rizkilabs <rizkilabs.dev@gmail.com>
2025-04-18 00:20:49 +0700

    add checkin, checkout & history route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AttendanceController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,5 +11,11 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/checkin', [AttendanceController::class, 'checkIn']);
+    Route::post('/checkout', [AttendanceController::class, 'checkOut']);
+    Route::get('/history', [AttendanceController::class, 'history']);
+});
 
 
