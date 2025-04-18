7c20eb673668d2555e3203043cef56eac68c614d
rizkilabs <rizkilabs.dev@gmail.com>
2025-04-18 23:42:44 +0700

    add export class

---
+++ app/Exports/AttendanceExport.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Attendance;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Contracts\Support\Responsable;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Http\Request;
+
+class AttendanceExport implements FromCollection, WithHeadings
+{
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        $query = Attendance::with('user')->orderBy('check_in', 'desc');
+
+        if($this->request->has('user_id')) {
+            $query->where('user_id', $this->request->user_id);
+        }
+
+        if($this->request->has('start_date') && $this->request->has('end_date')) {
+            $query->whereBetween('check_in', [
+                $this->request->input('start_date'),
+                $this->request->input('end_date')
+            ]);
+        }
+
+        return $query->get()->map(function ($item) {
+            return [
+                'Nama' => $item->user->name,
+                'Email' => $item->user->email,
+                'Check In' => $item->check_in,
+                'Check Out' => $item->check_out,
+                'Latitude' => $item->latitude,
+                'Longtitude' => $item->longtitude,
+            ];
+        });
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Nama',
+            'Email',
+            'Check In',
+            'Check Out',
+            'Latitude',
+            'Longtitude',
+        ];
+    }
+}
